ff6830311d0aaf76ebf9647eb1e87dbb2110e87c
stephen eshelman <objectthink@comcast.net>
2014-07-06 10:09:23 -0400

    fix constraints for segment view controller

---
+++ SignalsTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignalsTableTableViewController.h
+//  TRIOS-MOBILE
+//
+//  Created by stephen eshelman on 7/6/14.
+//  Copyright (c) 2014 objectthink.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignalsTableTableViewController : UITableViewController
+
+@end
